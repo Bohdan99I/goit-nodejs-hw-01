374ebdc1f77f1e98db1ef02cc9a814a82167494c
Bohdan99I <bohdan.voiyov@gmail.com>
2023-09-16 16:09:29 +0300

    contacts.json

---
+++ contacts.js
@@ -0,0 +1,12 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+function listContacts() {}
+
+function getContactById(contactId) {}
+
+function removeContact(contactId) {}
+
+function addContact(name, email, phone) {}

123270732369e352a12ae129081007acb845e0ba
Bohdan99I <bohdan.voiyov@gmail.com>
2023-09-24 18:35:38 +0300

    Fix

--- contacts.js
+++ contacts.js
@@ -6,6 +6,7 @@ const contactsPath = path.join(__dirname, "db/contacts.json");
 
 async function listContacts() {
   const data = await fs.readFile(contactsPath);
+  
   return JSON.parse(data);
 }
 
@@ -19,12 +20,11 @@ async function getContactById(contactId) {
 async function removeContact(contactId) {
   const contacts = await listContacts();
   const index = contacts.findIndex((contact) => contact.id === contactId);
-
   if (index === -1) {
     return null;
   }
-  const [deleteContact] = contacts.splice(index, 1);
 
+  const [deleteContact] = contacts.splice(index, 1);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return deleteContact;
 }
@@ -34,7 +34,6 @@ async function addContact(name, email, phone) {
   const newContact = { id: nanoid(), name, email, phone };
 
   contacts.push(newContact);
-
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return newContact;
 }
--- index.js
+++ index.js
@@ -18,15 +18,19 @@ async function invokeAction({ action, id, name, email, phone }) {
     case "list":
       const allContacts = await contacts.listContacts();
       return console.table(allContacts);
+
     case "get":
       const contactById = await contacts.getContactById(id);
       return console.log(contactById);
+
     case "add":
       const addContact = await contacts.addContact(name, email, phone);
       return console.log(addContact);
+
     case "remove":
       const deleteContact = await contacts.removeContact(id);
       return console.log(deleteContact);
+
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
